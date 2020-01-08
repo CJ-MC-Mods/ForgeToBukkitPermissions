f4eff6dcce63715fa8e07b4ec28aa92d5aab0c7c
CJ Miller <cjmiller721@gmail.com>
2020-01-08 00:15:41 -0800

    Update to 1.14.4

--- src/main/java/com/cjm721/forgetobukkitpermissions/ForgeToBukkitPermissions.java
+++ src/main/java/com/cjm721/forgetobukkitpermissions/ForgeToBukkitPermissions.java
@@ -1,23 +1,19 @@
 package com.cjm721.forgetobukkitpermissions;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.server.permission.PermissionAPI;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ForgeToBukkitPermissions.MODID, name = ForgeToBukkitPermissions.NAME, version = ForgeToBukkitPermissions.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
+@Mod(value = ForgeToBukkitPermissions.MODID)
 public class ForgeToBukkitPermissions
 {
   static final String MODID = "forgetobukkitpermissions";
-  static final String NAME = "Forge To Bukkit Permissions";
-  static final String VERSION = "1.0";
 
-  private static Logger logger;
+  private static Logger logger = LogManager.getLogger();
 
-  @Mod.EventHandler
-  public void preInit(FMLPreInitializationEvent event)
+  public ForgeToBukkitPermissions()
   {
-    logger = event.getModLog();
     PermissionAPI.setPermissionHandler(new BukkitPermissionsHandler());
     logger.info("Registered Forge API Permission Handler");
   }
